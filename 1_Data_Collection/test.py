0ec3b85851d0132054fd6190c7ae2e5296f8d05b
flannelmonke <khalilanaji@gmail.com>
2024-02-09 19:50:08 -0500

    add test script for file name formatting

--- 1_Data_Collection/test.py
+++ 1_Data_Collection/test.py
@@ -1,9 +1,15 @@
-links = open('links.txt', 'r')
+links = open('./GIT_NO/player_links.txt', 'r')
 
 links = links.readlines()
 
 i=0
 for link in links[0:100]:
     url = link.strip()
-    print(i, " " + url)
-    i += 1
+    
+    file_name = url.split('/')
+    file_name = file_name[len(file_name)-2]
+    file_name = file_name.split('-')
+    file_name[0] = file_name[0].capitalize()
+    file_name[1] = file_name[1].capitalize()
+    file_name = file_name[0] + file_name[1] + '.csv'
+    print(file_name)
